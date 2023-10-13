e556a95fccd9957ed00dc3312694641608a365d0
pratibhasingh47 <pratibhasingh1624@gmail.com>
2023-10-13 18:34:27 +0530

    scrabble

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -15,9 +15,11 @@ int main(void)
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
 
+
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
+
 
     // TODO: Print the winner
     if(score1 > score2)
@@ -36,7 +38,7 @@ int main(void)
 
 int compute_score(string word)
 {
-    
+
     // TODO: Compute and return score for string
     int n,x=0;
     n = strlen(word);
