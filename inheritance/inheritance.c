569c97985ef0331718ecb801d0d823e6728ad321
pratibhasingh47 <pratibhasingh1624@gmail.com>
2023-10-16 18:34:27 +0530

    scrabble

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -82,7 +82,6 @@ void free_family(person *p)
     {
         return;
     }
-
     // TODO: Free parents recursively
     free_family(p->parents[0]);
     free_family(p->parents[1]);
@@ -91,7 +90,6 @@ void free_family(person *p)
     free(p);
 
 }
-
 // Print each family member and their alleles.
 void print_family(person *p, int generation)
 {
@@ -130,9 +128,7 @@ void print_family(person *p, int generation)
     print_family(p->parents[1], generation + 1);
 }
 
-
 // Randomly chooses a blood type allele.
-
 char random_allele()
 {
     int r = rand() % 3;
