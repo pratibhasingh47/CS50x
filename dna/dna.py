f0c4777f607de0293d5c2ea75fabb61c16e7e6f1
pratibhasingh47 <pratibhasingh1624@gmail.com>
2023-11-20 18:34:27 +0530

    dna

--- dna/dna.py
+++ dna/dna.py
@@ -29,7 +29,6 @@ def main():
         for str in strs:
             seq_str_count[str] = longest_match(line, str)
 
-
     # TODO: Check database for matching profiles
     for profile in profiles:
         match_count = 0
