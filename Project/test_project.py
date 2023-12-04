8b154ac890453e95b8d514665180a0c0d6a54b96
pratibhasingh47 <pratibhasingh1624@gmail.com>
2023-12-04 18:34:27 +0530

    project

--- Project/test_project.py
+++ Project/test_project.py
@@ -14,7 +14,6 @@ def test_find_index():
         find_index(["2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18",["19","20"],["21","22","23","24","25"]], "189")
 
 
-
 def test_food_calories():
     assert food_calories("calories.csv")["Apple"]==52
 
