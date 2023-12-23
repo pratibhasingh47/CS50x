5dea93926a6dc75162e6dc750ad7d0c3d3daa8ab
pratibhasingh47 <pratibhasingh1624@gmail.com>
2023-12-23 18:34:27 +0530

    finance

--- finance/helpers.py
+++ finance/helpers.py
@@ -10,7 +10,6 @@ from flask import redirect, render_template, session
 from functools import wraps
 
 
-
 def apology(message, code=400):
     """Render message as an apology to user."""
     def escape(s):
